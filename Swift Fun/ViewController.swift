7cbab2296450cf5fc8987ddc78f103818c167bf2
Oscar Martinez <osky@oscars-macbook-pro.local>
2018-09-04 21:06:12 -0400

    Our First Commit as Gitters

--- Swift Fun/ViewController.swift
+++ Swift Fun/ViewController.swift
@@ -10,9 +10,39 @@ import UIKit
 
 class ViewController: UIViewController {
 
+
+    var buttonCount = 0
+    @IBAction func buttonTapped(_ sender: Any) {
+        view.backgroundColor = UIColor.red
+        
+        buttonCount = buttonCount + 1
+        
+        if buttonCount > 3 {
+            view.backgroundColor = UIColor.blue
+            
+        
+        }
+        
+        
+        
+        Mylabel.text = "Osky Is Cool"
+        
+        if buttonCount > 3 {
+            Mylabel.text = "STOP IT NOW"
+        }
+    }
+    
+    
+    @IBOutlet weak var Mylabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+       
+        
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
